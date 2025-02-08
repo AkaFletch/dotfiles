072121b538d2accfc050a43d9af4e3e04c515c96
Matt <matt@threlfall.dev>
2025-02-08 19:43:40 +0000

    feat: Adding code screenshotting

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -22,6 +22,20 @@ local plugins = {
   },
   { "nvim-neotest/nvim-nio" },
   {
+    "mistricky/codesnap.nvim", build = "make build_generator",
+    keys = {
+      { "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+      { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+    },
+    opts = {
+      save_path = "~/Pictures",
+      has_breadcrumbs = false,
+      bg_theme = "bamboo",
+      watermark = "",
+      mac_window_bar = false,
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     config=function ()
       require "plugins.configs.lspconfig"
